0a5a127c628d344d81b5c7d52f7e970f3de923b1
hiren9979 <hirenchavda141@gmail.com>
2023-08-07 17:56:51 +0530

    Initial commit

---
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int num1, num2;
+            Console.WriteLine("Write two number for sum : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("multiplication of " + num1 + " and " + num2 + " is : " + multiply(num1, num2));
+        }
+
+        //function for sum of two numbers
+        public static int multiply(int num1,int num2)
+        {
+            int total =  num1 * num2;
+            return total;
+        }
+    }
+}
